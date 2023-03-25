6d0ebd909608ba3534fcdb9f5deef3a6bc9a7f99
Kelvin Acosta <acosta.kelvin02@gmail.com>
2023-03-25 00:48:53 -0400

    complete loop and while lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+const myArr = [];
+function writeCards(name){
+    
+    for(let i = 0; i<name.length;i++){
+        const message = `Thank you, ${name[i]}, for the wonderful surprise gift!`;
+        
+        myArr.push(message);
+
+    }
+    return myArr;
+}
+function countDown(number){
+    while(number >= 0 ){
+        console.log(number);
+        number--;
+    }
+
+}
